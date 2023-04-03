4e5380481e2210a39e1dad696029dd495754a5ac
Dev-gojoy <32jongwoo@gmail.com>
2023-04-03 09:13:00 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello, React!</h1>
+      {/* 자바스크립트 리액트 문법은 className을 사용한다. */}
+      <h2 className="text-3xl font-bold underline">My Name is Ko Jong Woo</h2>
+      <h3 className="text-3xl font-bold underline">
+        My Favorite drink is Sprite 🥤
+      </h3>
+    </div>
+  );
+}
+
+export default App;
